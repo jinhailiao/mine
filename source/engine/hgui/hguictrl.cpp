2046fc8fb54ddf32d7a2266c1b416a437de96f63
jinhailiao <jinhailiao@163.com>
2017-08-04 15:43:25 +0800

    修正消息框获取按键消息不到的缺陷

--- source/engine/hgui/hguictrl.cpp
+++ source/engine/hgui/hguictrl.cpp
@@ -726,7 +726,7 @@ int C_BUTTON::WndProcess(S_WORD evt, S_WORD wParam, S_DWORD lParam)
 		m_state = HGUI_BS_PUSHBTN_DN;
 		m_pParent->SetCapture(this);
 		m_pParent->SendWndEvt(EVT_CTRLFOCUS, m_ID, 0);
-		m_pParent->SendWndEvt(EVT_COMMAND, m_ID, m_state);
+		ok = m_pParent->SendWndEvt(EVT_COMMAND, m_ID, m_state);
 		break;
 	case EVT_MOUSEUP:{
 		S_WORD x = HAI_GETLOWORD(lParam);
@@ -735,7 +735,7 @@ int C_BUTTON::WndProcess(S_WORD evt, S_WORD wParam, S_DWORD lParam)
 		m_state = HGUI_BS_PUSHBTN_UP;
 		m_pParent->SetCapture(NULL);
 		if (Rect.PointInRect(x, y) == true)
-			m_pParent->SendWndEvt(EVT_COMMAND, m_ID, m_state);
+			ok = m_pParent->SendWndEvt(EVT_COMMAND, m_ID, m_state);
 		}break;
 	case EVT_MOUSEDB:
 		m_pParent->SetCapture(NULL);
